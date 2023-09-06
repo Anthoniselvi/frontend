5972f69f40ebd4dbe37a1edff3b9fb58341540ed
Anthoniselvi <anthoniselvi33@gmail.com>
2023-09-06 15:21:40 +0530

    add event updated on dashboard page

--- src/pages/Topbar/Topbar.js
+++ src/pages/Topbar/Topbar.js
@@ -1,14 +1,38 @@
 import "./Topbar.css";
 import { useUserAuth } from "../../auth";
+import AddIcon from "@mui/icons-material/Add";
+import { useMediaQuery, Fab, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const Topbar = () => {
+  const [searchParam] = useSearchParams();
+  const profileId = searchParam.get("profile");
   const auth = useUserAuth();
-  // console.log("Name: " + auth.user.displayName);
+  const isNonMobile = useMediaQuery("(max-width:1000px)");
+  const navigate = useNavigate();
+  const navigateToCreateEvent = () => {
+    navigate(`/newevent?profile=${profileId}`);
+  };
   return (
     <div className="topbar">
       <div className="wrapper">
         <p className="wrapper-text">Welcome {auth.user.displayName} !</p>
       </div>
+      <Box
+        sx={{
+          position: "fixed",
+          top: isNonMobile && "90%",
+          bottom: "5%",
+          left: isNonMobile ? "90%" : "97%",
+          transform: "translateX(-50%)",
+          zIndex: 2000,
+        }}
+      >
+        <Fab color="secondary" aria-label="add" onClick={navigateToCreateEvent}>
+          <AddIcon style={{ fontSize: 25 }} />
+        </Fab>
+      </Box>
     </div>
   );
 };
